8a542d6a845ca01f6a50780b3efe4eb57c910fc6
LuisMatamoros14 <luismatamoroszarate@gmail.com>
2023-01-24 22:26:17 -0600

    Agregar archivos de proyecto.

---
+++ Tareas_MVC/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tareas_MVC.Entidades;
+
+namespace Tareas_MVC
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Tarea>().Property(t=>t.Titulo).HasMaxLength(250).IsRequired();
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+                public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+    }
+}
