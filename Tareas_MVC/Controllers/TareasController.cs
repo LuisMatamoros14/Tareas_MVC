b6b493c1379d1f87f45659bfa82d6860ae9b9c3a
LuisMatamoros14 <luismatamoroszarate@gmail.com>
2023-02-03 17:26:29 -0600

    Select para mostrar listado de tareas

--- Tareas_MVC/Controllers/TareasController.cs
+++ Tareas_MVC/Controllers/TareasController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Tareas_MVC.Entidades;
+using Tareas_MVC.Models;
 using Tareas_MVC.Servicios;
 
 namespace Tareas_MVC.Controllers
@@ -18,10 +19,18 @@ namespace Tareas_MVC.Controllers
         }
 
         [HttpGet]
-        public async Task<List<Tarea>> Get()
+        public async Task<List<TareaDTO>> Get()
         {
             var usuarioId = servicioUsuarios.ObtenerUsuarioId();
-            return await context.Tareas.Where(t=>t.UsuarioCreacionId==usuarioId).OrderBy(t=>t.Orden).ToListAsync();
+            var tareas = await context.Tareas.Where(t=>t.UsuarioCreacionId==usuarioId)
+                            .OrderBy(t=>t.Orden)
+                            .Select(t=> new TareaDTO
+                            {
+                                Id = t.Id,
+                                Titulo = t.Titulo
+                            })
+                            .ToListAsync();
+            return tareas;
         }
 
         [HttpPost]
