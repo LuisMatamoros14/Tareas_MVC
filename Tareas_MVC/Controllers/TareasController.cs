8be966c4ca809e3a6c29a133254889e7218c5719
LuisMatamoros14 <luismatamoroszarate@gmail.com>
2023-02-03 16:23:07 -0600

    Funcion de guardar tarea a BS en controlador

--- Tareas_MVC/Controllers/TareasController.cs
+++ Tareas_MVC/Controllers/TareasController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tareas_MVC.Entidades;
 using Tareas_MVC.Servicios;
 
 namespace Tareas_MVC.Controllers
@@ -14,5 +16,33 @@ namespace Tareas_MVC.Controllers
             this.context = context;
             this.servicioUsuarios = servicioUsuarios;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Tarea>> Post([FromBody] string titulo)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var existenTareas = await context.Tareas.AnyAsync(t => t.UsuarioCreacionId == usuarioId);
+
+            var ordenMayor = 0;
+            
+            if(existenTareas)
+            {
+                ordenMayor = await context.Tareas.Where(t=>t.UsuarioCreacionId == usuarioId).Select(t=> t.Orden).MaxAsync();
+            }
+
+            var tarea = new Tarea()
+            {
+                Titulo = titulo,
+                UsuarioCreacionId = usuarioId,
+                FechaCreacion = DateTime.UtcNow,
+                Orden = ordenMayor + 1
+            };
+
+            context.Add(tarea);
+            await context.SaveChangesAsync();
+
+            return tarea;
+        }
+
     }
 }
